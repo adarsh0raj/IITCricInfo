327713da6d682a3484b6b5ce525e5ed613355903
adarsh <rajadarsh2602@gmail.com>
2022-01-28 14:00:32 +0530

    modify

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from './app-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private appService: AppServiceService) {
+  }
+
+  ngOnInit(){
+    this.getDataFromApi();
+  }
+
+  getDataFromApi(){
+    this.appService.getData().subscribe((data) => {
+       console.log('Response from Api is:', data);
+    }, (error) => {
+      console.log('Error from Api is:', error);
+    })
+  }
+
 }
